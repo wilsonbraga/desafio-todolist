f7e96ffe35e3797f4f2f028c2b2d9d4536fe0580
Wilson Braga <wilsonbraga>
2025-02-26 13:47:34 -0300

    TodoService, metodos: create, list, update e delete

--- src/main/java/br/com/wilson/braga/desafio_todolist/service/TodoService.java
+++ src/main/java/br/com/wilson/braga/desafio_todolist/service/TodoService.java
@@ -1,13 +1,43 @@
 package br.com.wilson.braga.desafio_todolist.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import br.com.wilson.braga.desafio_todolist.entity.Todo;
+import br.com.wilson.braga.desafio_todolist.repository.TodoRepository;
 
 
 @Service
 public class TodoService {
     
+    private TodoRepository todoRepository;
 
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+       Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+       return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
 
 
 }
