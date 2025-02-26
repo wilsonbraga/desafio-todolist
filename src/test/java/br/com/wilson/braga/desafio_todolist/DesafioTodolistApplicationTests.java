50a6c1b18fcf8b3105bebe1fb4bffe045a41a2ec
Wilson Braga <wilsonbraga>
2025-02-26 12:20:11 -0300

    inicio do projeto

---
+++ src/test/java/br/com/wilson/braga/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wilson.braga.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
